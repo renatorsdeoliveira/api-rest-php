add414e60eaadd672ef4ce2ad13016b366fabe7d
Renato_oliveira <renatorsdeoliveira@gmail.com>
2020-03-25 11:41:35 -0300

    Contando os erros e salvando em um array

--- source/Controller/User.php
+++ source/Controller/User.php
@@ -2,14 +2,40 @@
 
     namespace Source\Controller;
 
-    require "../../vendor/autoload.php";
+use Source\Model\Validacao;
+
+require "../../vendor/autoload.php";
     require "../config.php";
     
     switch ($_SERVER["REQUEST_METHOD"]) {
         case  "POST":
                 $data = json_decode(file_get_contents("php://input"), false);
                 
-                var_dump($data);
+                if(!$data){
+                    header("HTTP/1.1 400 Bad Request");
+                    echo json_encode(array("respose" => "Metodo não encontrado"));
+                    exit;   
+                }
+
+                $erros = array();
+
+                if(!Validacao::validacaoString($data->first_name)){
+                    array_push($erros, "Nome informado invalido!");
+                }
+                if(!Validacao::validacaoString($data->last_name)){
+                    array_push($erros, "Sobrenome informado invalido!");
+                }
+                if(!Validacao::validacaoEmail($data->email)){
+                    array_push($erros, "Email informado invalido!");
+                }
+
+                if(count($erros)>0){
+                    header("HTTP/1.1 400 Bad Request");
+                    echo json_encode(array("respose" => "Há campos invalidos no formulario", "filds"=>$erros));
+                    exit;  
+                }
+                var_dump($erros);
+
             break;
         
         default:
